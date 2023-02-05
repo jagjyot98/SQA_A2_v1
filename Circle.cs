5d20af1d048a0f50b78ae7f3d66a9bd9f27348ef
Jagjyot Singh <jagjyot98@gmail.com>
2023-02-04 19:57:09 -0500

    Created Solution Project.

---
+++ Circle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQA_A2_v1
+{
+    public class Circle
+    {
+        private int radius;
+        public Circle()
+        {
+            radius = 1;
+        }
+
+        public Circle(int newRadius)
+        {
+            radius = newRadius;
+        }
+
+        public int GetRadius()
+        {
+            return radius;
+        }
+
+        public void SetRadius(int newRadius)
+        {
+            radius = newRadius;
+        }
+
+        public double GetCircumference()
+        {
+            return 2 * Math.PI * radius;
+        }
+
+        public double GetArea()
+        {
+            return Math.PI * radius * radius;
+        }
+    }
+}
